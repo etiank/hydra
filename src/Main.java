08460ee8b001ebf7d21658e83c84b91d97ccf79e
etiank <krizmanetian@protonmail.com>
2024-01-29 19:24:37 +0100

    Program finalized. It is in working condition. Used launch4j to turn it into a .exe - not included. I'm only including the JAR. Image used was created by me.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
 import java.awt.*;
@@ -6,10 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Random;
 
 public class Main {
@@ -21,7 +17,7 @@ public class Main {
     private static void createAndShowGUI() {
 
         JFrame frame = new JFrame("HYDRA");
-        ImageIcon icon = new ImageIcon("/resources/hydra.png");
+        ImageIcon icon = new ImageIcon(Main.class.getResource("/hydra.png"));
         frame.setIconImage(icon.getImage());
         frame.setSize(350, 120);
         JLabel label = new JLabel("<html>Cut off a head, two more will take its place.<br/><html/>");
@@ -30,19 +26,16 @@ public class Main {
         JPanel leftPanel = new JPanel(); leftPanel.setLayout(new FlowLayout());
         JPanel rightPanel = new JPanel(); rightPanel.setLayout(new GridLayout(2,1));
         JPanel rightSubPanel1 = new JPanel(); rightSubPanel1.setLayout(new FlowLayout());
-        JPanel rightSubPanel2 = new JPanel(); //rightSubPanel2.setLayout(new FlowLayout());
-
-        // Set layout manager to BorderLayout
+        JPanel rightSubPanel2 = new JPanel();
         frame.setLayout(new BorderLayout());
-
-        // Add label to the frame at the top
         frame.add(label, BorderLayout.NORTH);
         rightSubPanel1.add(label);
         rightSubPanel2.add(button, BorderLayout.EAST);
-        leftPanel.add(new JLabel(new ImageIcon("/resources/hydra.png")), (BorderLayout.WEST));
 
+        leftPanel.add(new JLabel(new ImageIcon(Main.class.getResource("/hydra.png"))));
 
 // ====================================================================
+
         rightPanel.add(rightSubPanel1);
         rightPanel.add(rightSubPanel2);
         frame.add(leftPanel, BorderLayout.WEST);
@@ -52,8 +45,6 @@ public class Main {
         int x = rand.nextInt(800);
         int y = rand.nextInt(600);
         frame.setLocation(x, y);
-        frame.setVisible(true);
-        frame.setResizable(false);
         CompoundBorder cb = new CompoundBorder(BorderFactory.createEmptyBorder(5,5,5,5), BorderFactory.createLineBorder(Color.BLACK));
         rightSubPanel1.setBorder(cb);
 
@@ -68,10 +59,11 @@ public class Main {
             }
         });
 
+        frame.setVisible(true);
+        frame.setResizable(false);
 
     }
 
-    // So this is the part that `recursively` runs hydra.jar file, effectively rendering task manager useless to end the task
     private static void Hydra(){
         try {
             String jarFilePath = "hydra.jar";
@@ -87,14 +79,5 @@ public class Main {
 
     }
 
-    // Trying to figure out how to make the image work in the outputted jar
-    /*private static BufferedImage loadImage(String path) {
-        try (InputStream inputStream = createAndShowGUI().class.getResourceAsStream(path)) {
-            return ImageIO.read(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }*/
 
 }
